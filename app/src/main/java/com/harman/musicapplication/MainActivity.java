a4540bde8f5f3e42c259b6d1a2a1fc7b7602cce6
ykumarvoora <25yogi12@gmail.com>
2022-09-20 18:38:26 +0530

    modified by adding seekbar

--- app/src/main/java/com/harman/musicapplication/MainActivity.java
+++ app/src/main/java/com/harman/musicapplication/MainActivity.java
@@ -5,21 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     // Instantiating the MediaPlayer class
     MediaPlayer music;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar= findViewById(R.id.seekBar2);
 
         // Adding the music file to our
         // newly created object music
         music = MediaPlayer.create(this, R.raw.sound);
+
+        seekBar.setMax(music.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(music.getCurrentPosition());
+            }
+        },0,900);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                music.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     // Playing the music
@@ -40,4 +71,5 @@ public class MainActivity extends AppCompatActivity {
         music.stop();
         music = MediaPlayer.create(this, R.raw.sound);
     }
+
 }
